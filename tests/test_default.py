dd8bdec340f34c8f637ae9951dcdfebad389d6d2
abhradip <abhradipm@gmail.com>
2017-05-30 00:12:22 +0000

    upload

---
+++ tests/test_default.py
@@ -0,0 +1,9 @@
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    '.molecule/ansible_inventory').get_hosts('all')
+
+
+def test_hosts_file(Package):
+    p = Package('apache2')
+    assert p.is_installed
