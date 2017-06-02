6bd287073d098e7abc50301c7f60ec7ef4a8603b
Administrator <admin@example.com>
2017-06-02 00:33:30 +0000

    removed test case

--- tests/test_default.py
+++ tests/test_default.py
@@ -4,6 +4,3 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_hosts_file(Package):
-    p = Package('apache2')
-    assert p.is_installed
